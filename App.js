3ac84bb3151e754bf1223eebcd2e2aa2bdcc6de3
Sania Saini <122249761+sania111@users.noreply.github.com>
2023-08-26 21:31:12 +0530

    Add files via upload
    TASK-1

---
+++ App.js
@@ -0,0 +1,20 @@
+import React,{ useState } from 'react';
+
+function App() {
+  const [count,setCount]= useState(0)
+  function decrementCount(){
+    setCount(prevCount=> prevCount-1 )
+  }
+  function incrementCount(){
+    setCount(prevCount=> prevCount+1 )
+  }
+  return (
+    <>
+    <button onClick={decrementCount}>-</button>
+    <span>{count}</span>
+    <button onClick={incrementCount}>+</button>
+    </>
+  );
+}
+
+export default App;
