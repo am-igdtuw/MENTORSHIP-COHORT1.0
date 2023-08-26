a8bd2045eaf1ddc5081a6cd98bb1c0f768306039
Arshita <122286846+Arshita-k@users.noreply.github.com>
2023-08-26 11:55:19 +0530

    Assignment1 Task2

---
+++ feed.js
@@ -0,0 +1,19 @@
+import sampleImages from './sample';
+import React from 'react';
+import './App.css'
+function App() {
+  return (
+    <><h1 className="page-heading"> Instagram </h1>
+    <hr className="line"/>
+    <div className="image-grid">
+      {sampleImages.map((item,index) => {
+        return (
+        <img key={index} 
+        src={item['product-image-url']} alt={'${index'}
+        className="image-item"/>
+        );
+      })}
+    </div></>
+  );
+}
+export default App;
