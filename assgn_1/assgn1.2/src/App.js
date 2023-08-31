e0efa7ddd3fb9b39d914bd4594f2449a8e9fe3e1
Semester 3 <sem3@Chhavis-MacBook-Pro.local>
2023-08-31 22:39:17 +0530

    assignment 2

--- assgn_1/assgn1.2/src/App.js
+++ assgn_1/assgn1.2/src/App.js
@@ -8,6 +8,7 @@ function App() {
   for (let i = 0; i < postData.length; i += 3) {
     const rowPosts = postData.slice(i, i + 3);
     const row = (
+      <div className='body'>
       <div className="row" key={i}>
         {rowPosts.map((post, index) => (
           <div className='col-md-4' key={index}>
@@ -19,6 +20,7 @@ function App() {
           />
           </div>
         ))}
+      </div>
       </div>
     );
     rows.push(row);
