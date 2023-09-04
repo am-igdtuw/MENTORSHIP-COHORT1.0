b377143d550d04f64178d1f76dbdac702c852d81
Saniya Verma <122946273+saniyaverma@users.noreply.github.com>
2023-09-04 19:07:39 +0530

    Update App.js

--- Assignment2_Task2/App.js
+++ Assignment2_Task2/App.js
@@ -1,35 +1,3 @@
-// function App({count}) { /*count is passed as a parameter*/
-//   return (
-//     <div className="App">
-//       {/* count = count +1 ;  NOT ALLOWED AS THIS IS PASSED AS PROP HERE*/} 
-//       <h1>Hello from functional Component</h1>
-//       <p>Count is: {count}</p>  {/*count is passed as a prop to this functional component*/} 
-//     </div>
-//   );
-// }
-
-// SECOND WAY OF PASSING PROPS:
-// function App(props) { /*count is passed as a parameter*/
-//   return (
-//     <div className="App">
-//       {/* count = count +1 ;  NOT ALLOWED AS THIS IS PASSED AS PROP HERE*/} 
-//       <h1>Hello from functional Component</h1>
-//       <p>Count is: {props.counter} {props.name}</p>  {/*count is passed as a prop to this functional component*/} 
-//     </div>
-//   );
-// }
-
-// After OBJECT DESTRUCTURING
-// function App({counter, name}) { /*count is passed as a parameter*/
-//   return (
-//     <div className="App">
-//       {/* count = count +1 ;  NOT ALLOWED AS THIS IS PASSED AS PROP HERE*/} 
-//       {/* <h1>Hello from functional Component</h1> */}
-//       <p>Count is: {counter} </p>  {/*count is passed as a prop to this functional component*/}
-//       <p>Made By {name}</p> 
-//     </div>
-//   );
-// }
 
 function ShowInfo({name,batch,course,email}) { 
   return (
