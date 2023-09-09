aacaa28bc07564f0f886ca63cdbc53ff1dd29991
shregupta89 <shregupta89@gmail.com>
2023-09-10 00:17:13 +0530

    Assignment 0_Shreya gupta

--- myreactapp2/src/App.js
+++ myreactapp2/src/App.js
@@ -1,25 +1,21 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import App1 from './functioncounter.jsx';
+import App2 from './classcounter.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  return(
+    <div>
+    <App1/>
+    
+    <App2/>
+    <p className="para"><b>Conclusion:</b>Both looks the same but the code is different!</p>
     </div>
-  );
+
+  )
 }
 
 export default App;
