6be7fceb15db31c7cfecda6e40b395275eb8bc9e
jiya10208 <jiya102082004@gmail.com>
2023-09-10 19:13:34 +0530

    changes

---
+++ assignment click me/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+const count = document.querySelector(".count");
+let clickcount = 0;
+
+button.addEventListener("click", function () {
+  clickcount += 1;
+  count.innerHTML = clickcount;
+});
